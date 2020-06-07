7bba3e3e3a58444d2c5c6b335b3cf4b44128c5d7
player1 <altsop@ttu.ee>
2020-06-07 22:06:51 +0300

    - init with frontend

---
+++ frontend/src/components/MainPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button @click="getBoobs">
+            Log in
+        </button>
+    </div>
+</template>
+
+<script>
+
+    import axios from 'axios';
+
+    export default {
+        name: "Fraud",
+        methods: {
+            async getBoobs() {
+                // const response = await axios.get('http://localhost:8080/messsage/default-message');
+                // console.log(response.data)
+                axios({
+                    method: 'get',
+                    url: '/default-message',
+                    headers: {
+                        "Authorization": "Basic YWRtaW46YWRtaW4="
+                    },
+                    responseType: 'json'
+                })
+            }
+        }
+    }
+</script>
