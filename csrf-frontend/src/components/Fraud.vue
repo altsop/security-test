bfc49c8f141e6f17880d004b7daf54cd9908f3c2
player1 <altsop@ttu.ee>
2020-06-07 22:33:51 +0300

    - init with csrf

---
+++ csrf-frontend/src/components/Fraud.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <a href="http://localhost:8080/messsage/default-message">
+            Free Icecream
+        </a>
+    </div>
+</template>
+
+<script>
+
+    import axios from 'axios';
+
+    export default {
+        name: "Fraud",
+        methods: {
+            async getBoobs() {
+                // const response = await axios.get('http://localhost:8080/messsage/default-message');
+                // console.log(response.data)
+                axios({
+                    method: 'get',
+                    url: 'http://localhost:8080/messsage/default-message',
+                })
+            }
+        }
+    }
+</script>
