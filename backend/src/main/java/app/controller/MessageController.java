f83cff5345305a2631d4ee9fe8cbefd25c32ad21
player1 <altsop@ttu.ee>
2020-06-07 22:03:10 +0300

    - init with backend

---
+++ backend/src/main/java/app/controller/MessageController.java
@@ -0,0 +1,17 @@
+package app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("messsage")
+public class MessageController {
+
+    @GetMapping("/default-message")
+    public String getDefaultMessage() {
+        System.out.println("------------------- csrf if here, dude -----------------------");
+        return "Default Message";
+    }
+
+}
